2d72c40f9d866306328ec60b6a0b936aac1edb1b
khcbsx-hue <khcbsx@gmail.com>
2026-04-24 16:24:07 +0700

    Update -serviceworker.js

--- -serviceworker.js
+++ -serviceworker.js
@@ -1,17 +1,38 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", (event) => {
-  if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      const newHeaders = new Headers(response.headers);
-      newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-      newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-      return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders,
-      });
-    })
-  );
-});
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window === 'undefined') {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+
+    self.addEventListener("message", ev => {
+        if (ev.data === "deregister") {
+            self.registration.unregister().then(() => {
+                self.clients.matchAll().then(clients => {
+                    clients.forEach(client => client.navigate(client.url))
+                });
+            });
+        }
+    });
+
+    self.addEventListener("fetch", function (event) {
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+            return;
+        }
+        
+        event.respondWith(fetch(event.request).then(response => {
+            if (response.status === 0) {
+                return response;
+            }
+
+            const newHeaders = new Headers(response.headers);
+            newHeaders.set("Cross-Origin-Embedder-Policy", coepCredentialless ? "credentialless" : "require-corp");
+            newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+            return new Response(response.body, {
+                status: response.status,
+                statusText: response.statusText,
+                headers: newHeaders
+            });
+        }).catch(e => console.error(e)));
+    });
+}
